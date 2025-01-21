0d39965bc6e486eae13b8fa85eff7e5fec4c86aa
Rohit Pal <rohitprakashpal@gmail.com>
2025-01-21 12:01:32 +0530

    added components

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <ul className="footer-links">
+          <li><a href="#join-our-team">Join our Team</a></li>
+          <li><a href="#contact-us">Contact Us</a></li>
+          <li><a href="#register">Register</a></li>
+          <li><a href="#our-policies">Our Policies</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Testimonials.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Testimonials() {
+  return (
+    <section className="testimonials">
+      <div className="ttml">
+        <h2>TESTIMONIALS</h2>
+        <h3>What our Users are saying </h3>
+        <h3>about us!!!</h3><br />
+        <div className="outfeed">
+          <div className="feedback">
+            <div className="user"><h4>Sakshi 26</h4></div>
+            <div className="user"><h4>Rashmi 23</h4></div>
+            <div className="user"><h4>Rashmi 25</h4></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonials;
