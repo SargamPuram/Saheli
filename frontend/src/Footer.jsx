e3b22bb3e602a2f25741bd1a9501286a48ee27ab
Rohit Pal <rohitprakashpal@gmail.com>
2025-01-24 14:57:51 +0530

    Updated link

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <ul className="footer-links">
-          <li><a href="#join-our-team">Join our Team</a></li>
-          <li><a href="#contact-us">Contact Us</a></li>
+          <li><a href="https://github.com/SargamPuram/Saheli">Join our Team</a></li>
+          <li><a href="https://github.com/rohitpal119/Saheli">Contact Us</a></li>
           
-          <li><a href="#our-policies">Our Policies</a></li>
+          <li><a href="https://cloud.ibm.com/docs/account?topic=account-iamusermanpol">Our Policies</a></li> 
         </ul>
       </div>
     </footer>
